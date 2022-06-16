d6c086134017b9bc28883cdd15cee8b57151e86a
ssryoung <qkrtpfud816@gmail.com>
2022-06-16 18:45:44 +0900

    master 복구

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 // import { useEffect } from 'react';
 // import axios from 'axios';
 import React from 'react';
@@ -62,17 +60,6 @@ function App() {
       ></Route>
       <Route path="/trackinfo" element={<TrackInfo />} />
     </Routes>
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
   );
 }
 
